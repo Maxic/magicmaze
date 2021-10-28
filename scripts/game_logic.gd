3ad7a14b563dbb2c3bbe461c267146e088684813
David Poot <david@Davids-MacBook-Pro.local>
2021-10-28 21:15:44 +0100

    fucked around with it and then a guy came and messed it

--- scripts/game_logic.gd
+++ scripts/game_logic.gd
@@ -6,14 +6,18 @@ enum phase {ENEMY_INTENTION, PLAYER_PHASE, ENEMY_ACTION}
 var current_phase
 var end_player_phase = false
 var end_enemy_action_phase = false
-var hero
+var hero_amount = 3
+var hero_array = []
+
 # Nodes
 onready var main = get_node("/root/main")
 
 func _ready():
 	Grid.create_grid()
-	hero = Hero.new(0,0)
-	main.add_child(hero)
+	for i in hero_amount:
+		i = Hero.new(i+3,0)
+		hero_array.append(i)
+		main.add_child(i)
 	
 	current_phase = phase.ENEMY_INTENTION
 	
@@ -21,16 +25,30 @@ func _physics_process(delta):
 
 	if current_phase == phase.ENEMY_INTENTION:
 		if true: # Skip tis for now
+			end_player_phase = false
 			current_phase = phase.PLAYER_PHASE
 			return
 	if current_phase == phase.PLAYER_PHASE:
 		if end_player_phase:
 			# setup next phase
-			var paths = Pathfinder.find_all_paths(hero.vec_pos, Grid.grid)
-			hero.remaining_path = paths[0]
+			for hero in hero_array:
+				var paths = Pathfinder.find_all_paths(hero.vec_pos, Grid.grid).duplicate()
+				hero.remaining_path = paths[0].duplicate()
+				hero.current_phase = hero.phase.WAITING
+			
+			end_enemy_action_phase = false	
 			current_phase = phase.ENEMY_ACTION
 			return
 	if current_phase == phase.ENEMY_ACTION:
+		for i in range(hero_array.size()):
+			var hero = hero_array[i]
+			if hero.current_phase == hero.phase.WAITING:
+				if i > 0 && hero_array[i-1].current_phase == hero.phase.DONE:
+					hero.current_phase = hero.phase.MOVING
+				elif i == 0:
+					hero.current_phase = hero.phase.MOVING
+				
+		
 		if end_enemy_action_phase:
-			current_phase = phase.PLAYER_PHASE
+			current_phase = phase.ENEMY_INTENTION
 			return
--- scripts/hero.gd
+++ scripts/hero.gd
@@ -4,10 +4,10 @@ class_name Hero
 var x
 var y
 var vec_pos
-var i = 0
 var type
 var remaining_path
-var hero_move = false
+enum phase {WAITING, MOVING, DONE}
+var current_phase
 var current_pos
 var new_pos
 
@@ -30,24 +30,25 @@ func _init(x_pos, y_pos):
 
 func _physics_process(delta):
 	if GameLogic.current_phase == GameLogic.phase.ENEMY_ACTION:
-		if remaining_path:
-			move_along_path(remaining_path)
-		else:
-			# TODO: add this hero's movement being done
-			GameLogic.end_enemy_action_phase = true
-	
+		if current_phase == phase.MOVING:
+			if remaining_path:
+				move_along_path(remaining_path)
+			else:
+				self.current_phase = phase.DONE
+
+		
 	
 func move_to_pos(x_pos, y_pos):
 	translation = Vector3(x_pos*2, translation.y, y_pos*2)
 	self.x = x_pos
 	self.y = y_pos
-	vec_pos = Vector2(x,y)
+	self.vec_pos = Vector2(x,y)
 	
 func move_along_path(path_arr):
 		remaining_path = path_arr
 		var pos = remaining_path[0]
 		new_pos = Vector3(pos.x*2,translation.y,pos.y*2)
-		self.translation = lerp(self.translation, new_pos, 0.03)
+		self.translation = lerp(self.translation, new_pos, 0.3)
 		if self.translation.x > new_pos.x-0.1 && self.translation.x < new_pos.x + 0.1 && \
 		self.translation.z > new_pos.z-0.1 && self.translation.z < new_pos.z + 0.1:
 			remaining_path.pop_front()
