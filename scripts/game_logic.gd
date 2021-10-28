54ca8342719654c36f4f70be434220c1d1c4f9ec
David Poot <david@Davids-MacBook-Pro.local>
2021-10-28 18:07:26 +0100

    added enemy_action phase logic

--- scripts/game_logic.gd
+++ scripts/game_logic.gd
@@ -5,12 +5,16 @@ enum phase {ENEMY_INTENTION, PLAYER_PHASE, ENEMY_ACTION}
 # state vars
 var current_phase
 var end_player_phase = false
-
+var end_enemy_action_phase = false
+var hero
 # Nodes
 onready var main = get_node("/root/main")
 
 func _ready():
 	Grid.create_grid()
+	hero = Hero.new(0,0)
+	main.add_child(hero)
+	
 	current_phase = phase.ENEMY_INTENTION
 	
 func _physics_process(delta):
@@ -25,13 +29,12 @@ func _physics_process(delta):
 	if current_phase == phase.PLAYER_PHASE:
 		# enable movement of indicator tile.
 		if end_player_phase: # TODO: Add correct condition
+			# setup next phase
+			var paths = Pathfinder.find_all_paths(hero.vec_pos, Grid.grid)
+			hero.remaining_path = paths[0]
 			current_phase = phase.ENEMY_ACTION
 			return
 	if current_phase == phase.ENEMY_ACTION:
-		var hero = Hero.new(0,0)
-		main.add_child(hero)
-		var paths = Pathfinder.find_all_paths(hero.vec_pos, Grid.grid)
-		hero.move_along_path(paths[0])
-		end_player_phase = false
-		current_phase = phase.PLAYER_PHASE
-		return
+		if end_enemy_action_phase:
+			current_phase = phase.PLAYER_PHASE
+			return
--- scripts/hero.gd
+++ scripts/hero.gd
@@ -29,25 +29,26 @@ func _init(x_pos, y_pos):
 	add_child(sprite)
 
 func _physics_process(delta):
-	if hero_move:
-		self.translation = lerp(self.translation, new_pos, 0.03)
-		if self.translation.x > new_pos.x-0.1 && self.translation.x < new_pos.x + 0.1 && \
-		   self.translation.z > new_pos.z-0.1 && self.translation.z < new_pos.z + 0.1:
-			if remaining_path:
-				move_along_path(remaining_path)
-			else:
-				hero_move = false
+	if GameLogic.current_phase == GameLogic.phase.ENEMY_ACTION:
+		if remaining_path:
+			move_along_path(remaining_path)
+		else:
+			# TODO: add this hero's movement being done
+			GameLogic.end_enemy_action_phase = true
 	
 	
 func move_to_pos(x_pos, y_pos):
 	translation = Vector3(x_pos*2, translation.y, y_pos*2)
 	self.x = x_pos
 	self.y = y_pos
-	
+	vec_pos = Vector2(x,y)
 	
 func move_along_path(path_arr):
 		remaining_path = path_arr
 		var pos = remaining_path[0]
 		new_pos = Vector3(pos.x*2,translation.y,pos.y*2)
-		remaining_path.pop_front()
-		hero_move = true;
+		self.translation = lerp(self.translation, new_pos, 0.03)
+		if self.translation.x > new_pos.x-0.1 && self.translation.x < new_pos.x + 0.1 && \
+		self.translation.z > new_pos.z-0.1 && self.translation.z < new_pos.z + 0.1:
+			remaining_path.pop_front()
+		
