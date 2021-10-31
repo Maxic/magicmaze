b39889ba0c16c3c3aa6fda7c0fbce304d8f1ead3
David Poot <david@Davids-MacBook-Pro.local>
2021-10-31 16:57:03 +0000

    ui system and event manager added

---
+++ scripts/UI.gd
@@ -0,0 +1,73 @@
+extends Control
+
+var t = 0.0
+var fade_in
+var fade_out
+var fade_in_time
+var fade_out_time
+var show_for_time
+var active_element
+
+var match_element
+var match_element_text
+
+var ui_action
+
+onready var phase_text_label = $TopWideContainer/VBoxContainer/phase_text
+onready var result_text_label = $CenterWideContainer/VBoxContainer/victory_text
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	
+	if ui_action:
+		
+		active_element = match_element
+
+		t += delta
+
+		if fade_in == true:
+			active_element.modulate.a += fade_in_time
+			if active_element.modulate.a >= 1:
+				active_element.modulate.a = 1
+				fade_in = false
+		if t >= show_for_time:
+			if fade_out:
+				active_element.modulate.a -= fade_out_time
+				if active_element.modulate.a <= 0:
+						active_element.modulate.a = 0
+						active_element.visible = false
+			else:		
+				active_element.visible = false
+						
+
+func set_ui_text(ui_element_name, ui_element_text):
+	match_element = find_ui_element(ui_element_name)
+	match_element.text = ui_element_text
+
+func fade_in_and_show_ui(ui_element_name, fade_time, show_time):
+	fade_in = true
+	fade_in_time = fade_time
+	show_for_time = show_time
+	match_element = find_ui_element(ui_element_name)
+	active_element = match_element
+	match_element.modulate.a = 0
+	match_element.visible = true
+	ui_action = true
+
+
+func fade_out_and_hide_ui(ui_element_name, fade_time):
+	fade_out = true
+	fade_out_time = fade_time
+	match_element = find_ui_element(ui_element_name)
+
+	
+func find_ui_element(ui_element_name):
+	match ui_element_name:
+		
+		"phase_text":
+			match_element = phase_text_label
+		"victory_text":
+			match_element = result_text_label
+	return match_element
---
+++ scripts/event_manager.gd
@@ -0,0 +1,27 @@
+extends Node
+
+onready var UI = get_node("/root/main/UI")
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+# 	UI.set_ui_text("name of UI node","text to be displayed")
+#	UI.fade_in_and_show_ui("name of UI node",alpha added per frame until the value is 1, total display time)
+#	UI.fade_out_and_hide_ui("name of UI node",alpha substracted per frame until value is 0)
+
+
+
+func hero_intention_phase_msg():
+	UI.set_ui_text("phase_text","The Heroes move!")
+	UI.fade_in_and_show_ui("phase_text",0.01,3)
+	UI.fade_out_and_hide_ui("phase_text",0.05)
